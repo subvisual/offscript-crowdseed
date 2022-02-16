b653ab71d45cf1f1d5d344b190e3a406d1b6ded7
Miguel Palhas <mpalhas@gmail.com>
2022-02-16 10:41:40 +0000

    Setup NFT deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import { task } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
+import "hardhat-deploy";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -12,8 +13,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const { DEV_MNEMONIC } = process.env;
+
 const config = {
   solidity: "0.8.11",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: DEV_MNEMONIC,
+      },
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
 };
 
 export default config;
