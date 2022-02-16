d9f8146841c0856aacedf1034f83a60df86da46a
Miguel Palhas <mpalhas@gmail.com>
2022-02-16 11:02:22 +0000

    Adding an empty create-react-app

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,14 @@
+import type { FC } from "react";
+
+import IndexView from "./views/IndexView";
+
+
+import "./App.css";
+
+const App: FC = () => {
+  return (
+    <></>
+  );
+};
+
+export default App;
