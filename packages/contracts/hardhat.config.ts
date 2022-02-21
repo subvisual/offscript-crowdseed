c7eab84d4f78682071f0b9f085d3fdc64f9a7f40
Miguel Palhas <mpalhas@gmail.com>
2022-02-21 19:10:52 +0000

    code review

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -16,6 +16,23 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const { DEV_MNEMONIC, TESTNET_MNEMONIC, INFURA_ETH_ID } = process.env;
 
+let networks: Record<string, any> = {
+  hardhat: {
+    accounts: {
+      mnemonic: DEV_MNEMONIC,
+    },
+  },
+};
+
+if (!process.env.CI) {
+  networks["rinkeby"] = {
+    url: `https://rinkeby.infura.io/v3/${INFURA_ETH_ID}`,
+    accounts: {
+      mnemonic: TESTNET_MNEMONIC,
+    },
+  };
+}
+
 const config = {
   solidity: {
     version: "0.8.11",
@@ -26,19 +43,7 @@ const config = {
       },
     },
   },
-  networks: {
-    hardhat: {
-      accounts: {
-        mnemonic: DEV_MNEMONIC,
-      },
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_ETH_ID}`,
-      accounts: {
-        mnemonic: TESTNET_MNEMONIC,
-      },
-    },
-  },
+  networks,
   namedAccounts: {
     deployer: 0,
   },
