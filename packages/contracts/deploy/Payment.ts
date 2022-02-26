545e42b4b00d1a8ffbc7b602cb383a899832db57
Miguel Palhas <mpalhas@gmail.com>
2022-02-26 13:07:24 +0000

    Fixing deploy configs

--- packages/contracts/deploy/Payment.ts
+++ packages/contracts/deploy/Payment.ts
@@ -18,7 +18,7 @@ const func: DeployFunction = async function (hre) {
   const { deploy, get } = hre.deployments;
 
   const config =
-    network.config.chainId == 1
+    network.name == "mainnet"
       ? {
           dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
           usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
