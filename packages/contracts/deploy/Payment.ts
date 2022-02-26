3ed0b356eb4f1d6c61484b7280437cf800f53400
Miguel Palhas <mpalhas@gmail.com>
2022-02-26 13:05:56 +0000

    Fixing deploy configs

--- packages/contracts/deploy/Payment.ts
+++ packages/contracts/deploy/Payment.ts
@@ -17,30 +17,26 @@ const func: DeployFunction = async function (hre) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy, get } = hre.deployments;
 
-  const configs: Record<number, Config> = {
-    // mainnet
-    1: {
-      dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-      usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-      usdt: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-      oracleDai: "0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9",
-      oracleEth: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
-      oracleUsdt: "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D",
-      oracleUsdc: "0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6",
-    },
-    // needs a mainnet fork
-    31337: {
-      dai: await getAddress(get, "FakeDAI"),
-      usdc: await getAddress(get, "FakeUSDC"),
-      usdt: await getAddress(get, "FakeUSDT"),
-      oracleEth: await getAddress(get, "FakeOracleETH"),
-      oracleDai: await getAddress(get, "FakeOracleUSD"),
-      oracleUsdt: await getAddress(get, "FakeOracleUSD"),
-      oracleUsdc: await getAddress(get, "FakeOracleUSD"),
-    },
-  };
-
-  const config = configs[network.config.chainId!];
+  const config =
+    network.config.chainId == 1
+      ? {
+          dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+          usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+          usdt: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+          oracleDai: "0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9",
+          oracleEth: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+          oracleUsdt: "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D",
+          oracleUsdc: "0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6",
+        }
+      : {
+          dai: await getAddress(get, "FakeDAI"),
+          usdc: await getAddress(get, "FakeUSDC"),
+          usdt: await getAddress(get, "FakeUSDT"),
+          oracleEth: await getAddress(get, "FakeOracleETH"),
+          oracleDai: await getAddress(get, "FakeOracleUSD"),
+          oracleUsdt: await getAddress(get, "FakeOracleUSD"),
+          oracleUsdc: await getAddress(get, "FakeOracleUSD"),
+        };
 
   const nft = await get("OffscriptNFT");
 
